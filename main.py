6ad88c362cfe3dcce5b88dcf05c565ccc5335a0a
Vakhtang Khukhunashvili <amareyah@gmail.com>
2021-01-11 18:40:56 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 def check_health(request):
   return "OK"
+
+def hello_pubsub(data, context):
+    if 'data' in data:
+        name = base64.b64decode(data['data']).decode('utf-8')
+    else:
+        name = 'world'
+    print(f'Hello {name}')
